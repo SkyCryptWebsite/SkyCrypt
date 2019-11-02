c6ff13a64d9ef7e60cb6d952f68ac0e1833c307a
LeaPhant <mail@lea.moe>
2019-11-02 02:49:32 +0100

    Add hat layer to 3D rendered heads

--- renderer.js
+++ renderer.js
@@ -56,21 +56,62 @@ function getPart(src, x, y, width, height, scale){
 
 module.exports = {
     renderHead: async (url, scale) => {
+        let hat_factor = 0.94;
+
         let canvas = createCanvas(scale * 20, scale * 18.5);
+        let hat_canvas = createCanvas(scale * 20, scale * 18.5);
+        let head_canvas = createCanvas(scale * 20 * hat_factor, scale * 18.5 * hat_factor);
+
         let ctx = canvas.getContext('2d');
+        let hat = hat_canvas.getContext('2d');
+        let head = head_canvas.getContext('2d');
 
         let skin = await loadImage(url);
 
-        let head_top = resize(removeTransparency(getPart(skin, 8, 0, 8, 8, 1)), scale);
-        let head_front = resize(removeTransparency(getPart(skin, 8, 8, 8, 8, 1)), scale);
-        let head_right = resize(removeTransparency(getPart(skin, 0, 8, 8, 8, 1)), scale);
+        let head_top = resize(removeTransparency(getPart(skin, 8, 0, 8, 8, 1)), scale * (hat_factor + 0.01));
+        let head_front = resize(removeTransparency(getPart(skin, 8, 8, 8, 8, 1)), scale * (hat_factor + 0.01));
+        let head_right = resize(removeTransparency(getPart(skin, 0, 8, 8, 8, 1)), scale * (hat_factor + 0.01));
+
+        let head_top_overlay, head_front_overlay, head_right_overlay;
 
         if(hasTransparency(getPart(skin, 32, 0, 32, 32, 1))){
             // render head overlay
-            head_top.getContext("2d").drawImage(getPart(skin, 40, 0, 8, 8, scale), 0, 0);
-            head_front.getContext("2d").drawImage(getPart(skin, 40, 8, 8, 8, scale), 0, 0);
-            head_right.getContext("2d").drawImage(getPart(skin, 32, 8, 8, 8, scale), 0, 0);
+            head_top_overlay = resize(getPart(skin, 40, 0, 8, 8, 1), scale);
+            head_front_overlay = resize(getPart(skin, 40, 8, 8, 8, 1), scale);
+            head_right_overlay =  resize(getPart(skin, 32, 8, 8, 8, 1), scale);
         }
+
+        if(head_top_overlay){
+            let x = 0;
+            let y = 0;
+            let z = 0;
+
+            let z_offset = scale * 3;
+            let x_offset = scale * 2;
+
+            // head top
+            x = x_offset;
+            y = -0.5;
+            z = z_offset;
+            hat.setTransform(1, -skew_a, 1, skew_a, 0, 0);
+            hat.drawImage(head_top_overlay, y - z, x + z, head_top_overlay.width, head_top_overlay.height + 1);
+
+            // head front
+            x = x_offset + 8 * scale;
+            y = 0;
+            z = z_offset - 0.5;
+            hat.setTransform(1, -skew_a, 0, skew_b, 0, skew_a);
+            hat.drawImage(head_front_overlay, y + x, x + z, head_front_overlay.width, head_front_overlay.height);
+
+            // head right
+            x = x_offset;
+            y = 0;
+            z = z_offset;
+            hat.setTransform(1, skew_a, 0, skew_b, 0, 0);
+            hat.drawImage(head_right_overlay, x + y, z - y - 0.5, head_right_overlay.width, head_right_overlay.height + 1);
+        }
+
+        scale *= hat_factor;
 
         let x = 0;
         let y = 0;
@@ -83,22 +124,26 @@ module.exports = {
         x = x_offset;
         y = -0.5;
         z = z_offset;
-        ctx.setTransform(1, -skew_a, 1, skew_a, 0, 0);
-        ctx.drawImage(head_top, y - z, x + z, head_top.width, head_top.height + 1);
+        head.setTransform(1, -skew_a, 1, skew_a, 0, 0);
+        head.drawImage(head_top, y - z, x + z, head_top.width, head_top.height + 1);
 
         // head front
         x = x_offset + 8 * scale;
         y = 0;
         z = z_offset - 0.5;
-        ctx.setTransform(1, -skew_a, 0, skew_b, 0, skew_a);
-        ctx.drawImage(head_front, y + x, x + z, head_front.width, head_front.height);
+        head.setTransform(1, -skew_a, 0, skew_b, 0, skew_a);
+        head.drawImage(head_front, y + x, x + z, head_front.width, head_front.height);
 
         // head right
         x = x_offset;
         y = 0;
         z = z_offset;
-        ctx.setTransform(1, skew_a, 0, skew_b, 0, 0);
-        ctx.drawImage(head_right, x + y, z - y - 0.5, head_right.width, head_right.height + 1);
+        head.setTransform(1, skew_a, 0, skew_b, 0, 0);
+        head.drawImage(head_right, x + y, z - y - 0.5, head_right.width, head_right.height + 1);
+
+
+        ctx.drawImage(head_canvas, (scale * 20 - scale * 20 * hat_factor) / 2, (scale * 18.5 - scale * 18.5 * hat_factor) / 2);
+        ctx.drawImage(hat_canvas, 0, 0);
 
         return await canvas.toBuffer('image/png');
     },
