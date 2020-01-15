8fe3bb046e99e84290ea865c1f2e6f30e5a92a53
LeaPhant <mail@lea.moe>
2020-01-15 12:08:31 +0100

    Implement custom texture animations

--- src/custom-resources.js
+++ src/custom-resources.js
@@ -3,17 +3,28 @@ const path = require('path');
 const mm = require('micromatch');
 const objectPath = require('object-path');
 const escapeRegExp = require('lodash.escaperegexp');
+const sharp = require('sharp');
+const { createCanvas, loadImage } = require('canvas');
+const mcData = require("minecraft-data")("1.8.9");
+const UPNG = require('upng-js');
+const RJSON = require('relaxed-json');
+
+const NORMALIZED_SIZE = 128;
+
+const RESOURCE_PACK_FOLDER = path.resolve(__dirname, '..', 'public', 'resourcepacks');
 
 let removeFormatting = new RegExp('§[0-9a-z]{1}', 'g');
 
-function getFiles(dir, fileList){
-    const files = fs.readdirSync(dir);
+async function getFiles(dir, fileList){
+    const files = await fs.readdir(dir);
 
     fileList = fileList || [];
 
     for(const file of files){
-        if(fs.statSync(path.resolve(dir, file)).isDirectory())
-            fileList = getFiles(path.resolve(dir, file), fileList);
+        let fileStat = await fs.stat(path.resolve(dir, file));
+
+        if(await fileStat.isDirectory())
+            fileList = await getFiles(path.resolve(dir, file), fileList);
         else
             fileList.push(path.resolve(dir, file));
     }
@@ -21,102 +32,235 @@ function getFiles(dir, fileList){
     return fileList;
 }
 
-let resourcePacks = [];
+function getFrame(src, frame){
+    const dst = createCanvas(NORMALIZED_SIZE, NORMALIZED_SIZE);
+    const ctx = dst.getContext("2d");
 
- for(const pack of fs.readdirSync('../resourcepacks')){
-    let basePath = path.resolve('../resourcepacks', pack);
-    let config = require(path.resolve(basePath, 'config.json'));
+    ctx.drawImage(src, 0, frame * NORMALIZED_SIZE * -1);
 
-    resourcePacks.push({
-        basePath,
-        config
-    });
+    return dst;
 }
 
-resourcePacks = resourcePacks.sort((a, b) => a.config.priority - b.config.priority);
+let resourcePacks = [];
 
-for(let pack of resourcePacks){
-    pack.files = getFiles(path.resolve(pack.basePath, 'assets', 'minecraft', 'mcpatcher', 'cit'));
-    pack.textures = [];
+async function init(){
+     for(const pack of await fs.readdir(RESOURCE_PACK_FOLDER)){
+        let basePath = path.resolve(RESOURCE_PACK_FOLDER, pack);
+        let config = require(path.resolve(basePath, 'config.json'));
 
-    for(const file of pack.files){
-        if(path.extname(file) != '.properties')
-            continue;
+        resourcePacks.push({
+            basePath,
+            config
+        });
+    }
 
-        let lines = fs.readFileSync(file, 'utf8').split("\r\n");
-        let properties = {};
+    resourcePacks = resourcePacks.sort((a, b) => a.config.priority - b.config.priority);
 
-        for(let line of lines){
-            let split = line.split("=");
+    for(let pack of resourcePacks){
+        pack.files = await getFiles(path.resolve(pack.basePath, 'assets', 'minecraft', 'mcpatcher', 'cit'));
+        pack.textures = [];
 
-            if(split.length < 2)
+        for(const file of pack.files){
+            if(path.extname(file) != '.properties')
                 continue;
 
-            properties[split[0]] = split.slice(1).join("=");
-        }
+            let lines = fs.readFileSync(file, 'utf8').split("\r\n");
+            let properties = {};
 
-        if(!('type' in properties))
-            continue;
+            for(let line of lines){
+                let split = line.split("=");
 
-        if(!properties.type == 'item')
-            continue;
+                if(split.length < 2)
+                    continue;
 
-        let texture = {weight: 0, match: []};
-
-        let textureFile = 'texture' in properties
-        ? path.resolve(path.dirname(file), properties.texture)
-        : path.resolve(path.dirname(file), path.basename(file, '.properties') + '.png');
-
-        texture.path = textureFile;
-
-        for(let property in properties){
-            if(property == 'weight')
-                texture.weight = parseInt(properties[property]);
-
-            if(!property.startsWith('nbt.'))
-                continue;
-
-            let regex = properties[property];
-
-            if(regex.startsWith('ipattern:')){
-                regex = mm.makeRe(regex.substring(9), { nocase: true });
-            }else if(regex.startsWith('pattern:')){
-                regex = mm.makeRe(regex.substring(9));
-            }else if(regex.startsWith('iregex:')){
-                regex = new RegExp(regex.substring(7), 'i');
-            }else if(regex.startsWith('regex:')){
-                regex = new RegExp(regex.substring(6));
-            }else{
-                regex = new RegExp(escapeRegExp(regex));
+                properties[split[0]] = split.slice(1).join("=");
             }
 
-            texture.match.push({
-                value: property.substring(4),
-                regex
-            });
-        }
+            if(!('type' in properties))
+                continue;
 
-        let mcMeta = fs.existsSync(textureFile + '.mcmeta') ? fs.readFileSync(textureFile + '.mcmeta', 'utf8') : false;
-        let metaProperties = {};
+            if(!properties.type == 'item')
+                continue;
 
-        if(mcMeta){
+            let texture = {weight: 0, animated: false, file: path.basename(file), match: []};
+
+            let textureFile = 'texture' in properties
+            ? path.resolve(path.dirname(file), properties.texture)
+            : path.resolve(path.dirname(file), path.basename(file, '.properties') + '.png');
+
+            if('texture.bow_standby' in properties)
+                textureFile = path.resolve(path.dirname(file), properties['texture.bow_standby']);
+
             try{
-                metaProperties = JSON.parse(mcMeta);
+                await fs.access(textureFile, fs.F_OK);
             }catch(e){
-
+                continue;
             }
+
+            texture.path = textureFile;
+
+            const textureImage = sharp(textureFile);
+            const textureMetadata = await textureImage.metadata();
+
+            if(textureMetadata.width != NORMALIZED_SIZE)
+                await
+                    fs.writeFile(textureFile, await
+                        textureImage
+                        .resize(NORMALIZED_SIZE, textureMetadata.height * (NORMALIZED_SIZE / textureMetadata.width), {
+                            kernel: sharp.kernel.nearest
+                        })
+                        .toBuffer()
+                    );
+
+            if(UPNG.decode(await fs.readFile(textureFile)).frames.length > 0)
+                texture.animated = true;
+
+            for(let property in properties){
+                if(property == 'weight')
+                    texture.weight = parseInt(properties[property]);
+
+                if(property == 'items'){
+                    let item = mcData.findItemOrBlockByName(properties[property].replace('minecraft:', ''));
+
+                    if(item)
+                        texture.id = item.id;
+                }
+
+                if(!property.startsWith('nbt.'))
+                    continue;
+
+                let regex = properties[property];
+
+                if(regex.startsWith('ipattern:')){
+                    regex = mm.makeRe(regex.substring(9), { nocase: true });
+                }else if(regex.startsWith('pattern:')){
+                    regex = mm.makeRe(regex.substring(9));
+                }else if(regex.startsWith('iregex:')){
+                    regex = new RegExp(regex.substring(7), 'i');
+                }else if(regex.startsWith('regex:')){
+                    regex = new RegExp(regex.substring(6));
+                }else{
+                    regex = new RegExp(escapeRegExp(regex));
+                }
+
+                texture.match.push({
+                    value: property.substring(4),
+                    regex
+                });
+            }
+
+            let mcMeta;
+
+            try{
+                mcMeta = await fs.readFile(textureFile + '.mcmeta', 'utf8');
+            }catch(e){
+                mcMeta = false;
+            }
+
+            let metaProperties = {};
+
+            if(mcMeta){
+                try{
+                    metaProperties = RJSON.parse(mcMeta);
+                }catch(e){
+                    //
+                }
+            }
+
+            if('animation' in metaProperties && textureMetadata.width != textureMetadata.height){
+                texture.animated = true;
+
+                const { animation } = metaProperties;
+                const canvas = createCanvas(NORMALIZED_SIZE, NORMALIZED_SIZE);
+                const ctx = canvas.getContext('2d');
+
+                const image = await loadImage(textureFile);
+
+                const pngFrames = [];
+                const pngDelays = [];
+
+                if('frames' in animation){
+                    let frameCount = animation.frames.length;
+                    let frameTime = animation.frametime / 20 * 1000;
+
+                    for(const frame of animation.frames){
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                        if(typeof frame == 'object'){
+                            if(!('index' in frame) && !('time' in frame))
+                                continue;
+
+                            ctx.drawImage(getFrame(image, frame.index), 0, 0);
+
+                            pngDelays.push(frame.time / 20 * 1000);
+                        }else if(typeof frame == 'number'){
+                            ctx.drawImage(getFrame(image, frame), 0, 0);
+
+                            pngDelays.push(frameTime);
+                        }
+
+                        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data.buffer;
+
+                        pngFrames.push(imageData);
+                    }
+                }else{
+                    let frameCount = image.height / NORMALIZED_SIZE;
+                    let frameCountInterpolated = frameCount;
+                    let frameTime = animation.frametime / 20 * 1000;
+                    let frameTimeInterpolated = frameTime;
+
+                    if(animation.interpolate){
+                        frameTimeInterpolated = 2 / 20 * 1000;
+                        frameCountInterpolated = frameCount * (frameTime / frameTimeInterpolated);
+                    }
+
+                    for(let i = 0; i < frameCountInterpolated; i++){
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                        let frameCur = Math.floor(i / frameCountInterpolated * frameCount) - 1;
+                        let frameNext = Math.floor(i / frameCountInterpolated * frameCount);
+                        let opacity = (i / frameCountInterpolated * frameCount) - frameCur - 1;
+
+                        if(frameCur < 0)
+                            frameCur = frameCount - 1;
+
+                        ctx.globalAlpha = 1;
+                        ctx.drawImage(getFrame(image, frameCur), 0, 0);
+
+                        ctx.globalAlpha = opacity;
+                        ctx.drawImage(getFrame(image, frameNext), 0, 0);
+
+                        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data.buffer;
+
+                        pngFrames.push(imageData);
+                        pngDelays.push(frameTimeInterpolated);
+                    }
+                }
+
+                if(pngFrames.length > 0){
+                    const apng = UPNG.encode(pngFrames, NORMALIZED_SIZE, NORMALIZED_SIZE, 0, pngDelays);
+
+                    await fs.writeFile(textureFile, Buffer.from(apng));
+                }
+            }
+
+            pack.textures.push(texture);
         }
-
-        if('animation' in metaProperties){
-
-        }
-
-        pack.textures.push(texture);
     }
 }
 
+const readyPromise = init();
+
+readyPromise.then(() => {
+    module.exports.ready = true;
+});
+
 module.exports = {
-    getTexture: nbt => {
+    ready: false,
+    getTexture: async item => {
+        if(!module.exports.ready)
+            await readyPromise;
+
         let outputTexture = { weight: -9999 };
 
         for(const pack of resourcePacks){
@@ -124,6 +268,9 @@ module.exports = {
                 outputTexture.weight = -9999;
 
             for(const texture of pack.textures){
+                if(texture.id != item.id)
+                    continue;
+
                 let matches = 0;
 
                 for(const match of texture.match){
@@ -132,16 +279,16 @@ module.exports = {
                     if(value.endsWith('.*'))
                         value = value.substring(0, value.length - 2);
 
-                    if(!objectPath.has(nbt, 'tag.' + value))
+                    if(!objectPath.has(item, 'tag.' + value))
                         continue;
 
-                    let matchValues = objectPath.get(nbt, 'tag.' + value);
+                    let matchValues = objectPath.get(item, 'tag.' + value);
 
                     if(!Array.isArray(matchValues))
                         matchValues = [matchValues];
 
                     for(const matchValue of matchValues){
-                        if(!regex.test(matchValue.replace(removeFormatting, '')))
+                        if(!regex.test(matchValue.toString().replace(removeFormatting, '')))
                             continue;
 
                         matches++;
@@ -149,7 +296,10 @@ module.exports = {
                 }
 
                 if(matches == texture.match.length){
-                    if(texture.weight <= outputTexture.weight)
+                    if(texture.weight < outputTexture.weight)
+                        continue;
+
+                    if(texture.weight == outputTexture.weight && texture.file < outputTexture.file)
                         continue;
 
                     outputTexture = Object.assign({}, texture);
@@ -159,6 +309,8 @@ module.exports = {
 
         if(!('path' in outputTexture))
             return null;
+
+        outputTexture.path = path.relative(path.resolve(__dirname, '..', 'public'), outputTexture.path);
 
         return outputTexture;
     }
