bf59f2a880b1ef82ebbdd1b433f2ceb086f687fe
LeaPhant <mail@lea.moe>
2020-01-15 12:07:42 +0100

    Update resourcepacks

--- public/resources/js/stats.js
+++ public/resources/js/stats.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
                 if(item.texture_path){
                     inventoryItemIcon.className += ' custom-icon';
-                    inventoryItemIcon.style.backgroundImage = 'url(' + item.texture_path + ')';
+                    inventoryItemIcon.style.backgroundImage = "url('" + item.texture_path + "')";
                 }
 
                 if(isEnchanted(item))
@@ -176,7 +176,7 @@ document.addEventListener('DOMContentLoaded', function(){
         itemNameContent.innerHTML = item.display_name || 'null';
 
         if(item.texture_path){
-            itemIcon.style.backgroundImage = 'url(' + item.texture_path + ')';
+            itemIcon.style.backgroundImage = "url('" + item.texture_path + "')";
             itemIcon.className = 'stats-piece-icon item-icon custom-icon';
         }else{
             itemIcon.removeAttribute('style');
@@ -203,7 +203,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
                     if(backpackItem.texture_path){
                         inventoryItemIcon.className += ' custom-icon';
-                        inventoryItemIcon.style.backgroundImage = 'url(' + backpackItem.texture_path + ')';
+                        inventoryItemIcon.style.backgroundImage = "url('" + backpackItem.texture_path + "')";
                     }
 
                     inventoryItemCount.className = 'item-count';
