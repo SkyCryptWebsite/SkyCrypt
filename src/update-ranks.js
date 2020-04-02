f03c1a6b6e680cd503ced2d7f5d07a38e93e4f13
LeaPhant <mail@lea.moe>
2020-04-02 19:18:47 +0200

    Add script to update ranks

---
+++ src/update-ranks.js
@@ -0,0 +1,40 @@
+const cluster = require('cluster');
+
+async function main(){
+    const dbUrl = 'mongodb://localhost:27017';
+    const dbName = 'sbstats';
+
+    const { MongoClient } = require('mongodb');
+    const axios = require('axios');
+    const credentials = require('../credentials.json');
+    const mongo = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    await mongo.connect();
+
+    const db = mongo.db(dbName);
+
+    async function updateRanks(){
+        let rank = 1;
+
+        const cursor = await db.collection('profileViews').find().sort({ total: -1 });
+
+        while(await cursor.hasNext()){
+            const doc = await cursor.next();
+
+            await db
+            .collection('profileViews')
+            .updateOne(
+                { uuid: doc.uuid },
+                { $set: { rank } }
+            );
+
+            rank++;
+        }
+    }
+
+    await updateRanks();
+
+    mongo.close();
+}
+
+if(cluster.isMaster)
+    main();
