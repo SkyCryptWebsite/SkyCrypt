f7671d4c477302906b856886be6669d750720e25
LeaPhant <mail@lea.moe>
2020-04-04 10:32:52 +0200

    Add index creation to init script

--- init.js
+++ init.js
@@ -8,3 +8,28 @@ if(!fs.existsSync('credentials.json'))
     fs.writeFileSync('credentials.json', JSON.stringify(credentials, null, 4));
 
 fs.ensureDirSync('cache');
+
+async function main(){
+    const dbUrl = 'mongodb://localhost:27017';
+    const dbName = 'sbstats';
+
+    const { MongoClient } = require('mongodb');
+    const mongo = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    await mongo.connect();
+
+    const db = mongo.db(dbName);
+
+    await db
+    .collection('profiles')
+    .createIndex(
+        { username: 'text' }
+    );
+
+    await db
+    .collection('usernames')
+    .createIndex(
+        { username: 'text' }
+    );
+}
+
+main();
