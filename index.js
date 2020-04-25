0f6f1a132e13e698d4248abecf2aa5df740b4d6b
LeaPhant <mail@lea.moe>
2020-04-25 21:25:48 +0200

    Add API_REQUESTS env variable to track api requests used per minute

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ require('./src/update-guilds');
 require('./src/update-bazaar');
 require('./src/update-items');
 //require('./src/update-ranks');
+
+const cluster = require('cluster');
+
+if(cluster.isMaster && process.env.API_REQUESTS == "1"){
+    let requests = [];
+
+    require('axios-debug-log')({
+        request: (debug, config) => {
+            if(config.baseURL == 'https://api.hypixel.net/')
+                requests.push(+new Date());
+        }
+    });
+
+    setInterval(() => {
+        requests = requests.filter(a => a > +new Date() - 60 * 1000);
+
+        console.log('requests:', requests.length + ' / minute');
+    }, 1000);
+}
