771ac4653d38c87d7383c83586510811fc1ee69a
LeaPhant <mail@lea.moe>
2020-06-28 14:59:24 +0200

    Move function to update leaderboard positions to lib

--- src/scripts/update-leaderboards.js
+++ src/scripts/update-leaderboards.js
@@ -6,171 +6,58 @@ async function main(){
 
     const { MongoClient } = require('mongodb');
     const _ = require('lodash');
-    const redis = require('async-redis');
 
     const helper = require('./../helper');
     const lib = require('./../lib');
     const constants = require('./../constants');
+
+    const ProgressBar = require('progress');
 
     const mongo = new MongoClient(dbUrl, { useUnifiedTopology: true });
     await mongo.connect();
 
     const db = mongo.db(dbName);
 
+    const redis = require('async-redis');
     const redisClient = redis.createClient();
 
-    function getMinMax(profiles, min, ...path){
-        let output = null;
-
-        const compareValues = profiles.map(a => helper.getPath(a, ...path)).filter(a => a !== undefined);
-
-        if(compareValues.length == 0)
-            return output;
-
-        if(min)
-            output = Math.min(...compareValues);
-        else
-            output = Math.max(...compareValues);
-
-        if(isNaN(output))
-            return null;
-
-        return output;
-    }
-
-    function getMax(profiles, ...path){
-        return getMinMax(profiles, false, ...path);
-    }
-
-    function getMin(profiles, ...path){
-        return getMinMax(profiles, true, ...path);
-    }
-
-    function getAllKeys(profiles, ...path){
-        return _.uniq([].concat(...profiles.map(a => _.keys(helper.getPath(a, ...path)))));
-    }
-
     async function updateLeaderboards(){
+        const keys = await redisClient.keys('lb_*');
+
+        let updateUsers = [];
+
+        for(const key of keys){
+            const lb = constants.leaderboard(key);
+
+            if(lb.sortedBy < 0)
+                updateUsers.push(...await redisClient.zrevrange([key, 0, 99]))
+            else
+                updateUsers.push(...await redisClient.zrange([key, 0, 99]))
+        }
+
+        updateUsers = _.uniq(updateUsers);
+
+        console.log('updating', updateUsers.length, 'profiles');
+
+        const bar = new ProgressBar('  generating leaderboards [:bar] :rate users/s :percent :etas', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: await db.collection('profileStore').estimatedDocumentCount()
+        });
+
         for await(const doc of db.collection('profileStore').find()){
             const { uuid } = doc;
 
-            console.log('trying to update', uuid);
+            bar.tick();
 
             try{
-                const { profile, allProfiles } = await helper.getProfile(db, uuid, null, { cacheOnly: true });
-                const hypixelProfile = await helper.getRank(uuid, db, true);
-
-                const memberProfiles = [];
-
-                for(const singleProfile of allProfiles){
-                    const userProfile = singleProfile.members[uuid];
-
-                    userProfile.levels = await lib.getLevels(userProfile, hypixelProfile);
-
-                    let totalSlayerXp = 0;
-
-                    userProfile.slayer_xp = 0;
-
-                    if(userProfile.hasOwnProperty('slayer_bosses')){
-                        for(const slayer in userProfile.slayer_bosses)
-                            totalSlayerXp += userProfile.slayer_bosses[slayer].xp;
-
-                        userProfile.slayer_xp = totalSlayerXp;
-
-                        for(const mountMob in constants.mob_mounts){
-                            const mounts = constants.mob_mounts[mountMob];
-
-                            userProfile.stats[`kills_${mountMob}`] = 0;
-                            userProfile.stats[`deaths_${mountMob}`] = 0;
-
-                            for(const mount of mounts){
-                                userProfile.stats[`kills_${mountMob}`] += userProfile.stats[`kills_${mount}`] || 0;
-                                userProfile.stats[`deaths_${mountMob}`] += userProfile.stats[`deaths_${mount}`] || 0;
-
-                                delete userProfile.stats[`kills_${mount}`];
-                                delete userProfile.stats[`deaths_${mount}`]
-                            }
-                        }
-                    }
-
-                    userProfile.pet_score = 0;
-
-                    const maxPetRarity = {};
-
-                    if(Array.isArray(userProfile.pets)){
-                        for(const pet of userProfile.pets)
-                            maxPetRarity[pet.type] = Math.max(maxPetRarity[pet.type] || 0, constants.pet_value[pet.tier.toLowerCase()]);
-
-                        for(const key in maxPetRarity)
-                            userProfile.pet_score += maxPetRarity[key];
-                    }
-
-                    memberProfiles.push({
-                        profile_id: singleProfile.profile_id,
-                        data: userProfile
-                    });
-                }
-
-                const values = {};
-
-                values['pet_score'] = getMax(memberProfiles, 'data', 'pet_score');
-
-                values['fairy_souls'] = getMax(memberProfiles, 'data', 'fairy_souls_collected');
-                values['average_level'] = getMax(memberProfiles, 'data', 'levels', 'average_level');
-                values['total_skill_xp'] = getMax(memberProfiles, 'data', 'levels', 'total_skill_xp');
-
-                for(const skill of getAllKeys(memberProfiles, 'data', 'levels', 'levels'))
-                    values[`skill_${skill}_xp`] = getMax(memberProfiles, 'data', 'levels', 'levels', skill, 'xp');
-
-                values['slayer_xp'] = getMax(memberProfiles, 'data', 'slayer_xp');
-
-                for(const slayer of getAllKeys(memberProfiles, 'data', 'slayer_bosses')){
-                    for(const key of getAllKeys(memberProfiles, 'data', 'slayer_bosses', slayer)){
-                        if(!key.startsWith('boss_kills_tier'))
-                            continue;
-
-                        const tier = key.split("_").pop();
-
-                        values[`${slayer}_slayer_boss_kills_tier_${tier}`] = getMax(memberProfiles, 'data', 'slayer_bosses', slayer, key);
-                    }
-
-                    values[`${slayer}_slayer_xp`] = getMax(memberProfiles, 'data', 'slayer_bosses', slayer, 'xp');
-                }
-
-                for(const item of getAllKeys(memberProfiles, 'data', 'collection'))
-                    values[`collection_${item.toLowerCase()}`] = getMax(memberProfiles, 'data', 'collection', item);
-
-                for(const stat of getAllKeys(memberProfiles, 'data', 'stats'))
-                    values[stat] = getMax(memberProfiles, 'data', 'stats', stat);
-
-                for(const key in values){
-                    if(values[key] == null)
-                        continue;
-
-                    await redisClient.zadd([`lb_${key}`, values[key], uuid]);
-                }
-
-                for(const singleProfile of allProfiles){
-                    if(helper.hasPath(singleProfile, 'banking', 'balance'))
-                        await redisClient.zadd([`lb_bank`, singleProfile.banking.balance, singleProfile.profile_id]);
-
-                    const minionCrafts = [];
-
-                    for(const member in singleProfile.members)
-                        if(Array.isArray(singleProfile.members[member].crafted_generators))
-                            minionCrafts.push(...singleProfile.members[member].crafted_generators);
-
-                    await redisClient.zadd([
-                        `lb_unique_minions`,
-                        _.uniq(minionCrafts).length,
-                        singleProfile.profile_id
-                    ]);
-                }
-
-                console.log('updated leaderboard for', uuid);
+                await lib.getProfile(db, uuid);
             }catch(e){
                 console.error(e);
             }
+
+            await new Promise(r => setTimeout(r, 500));
         }
 
         console.log('done updating leaderboards');
