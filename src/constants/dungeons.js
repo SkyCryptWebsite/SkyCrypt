56917722a5120151c994e589282983c4498459ce
MartinNemi03 <mrmartinek@email.cz>
2020-08-05 04:18:58 +0200

    Added some example constants for boss collections

---
+++ src/constants/dungeons.js
@@ -0,0 +1,20 @@
+module.exports = {
+    boss_collections: {
+        "bonzo": {
+            name: "Bonzo",
+            texture: "",
+            rewards: {
+                "red_nose": {
+                    name: "Red Nose",
+                    required: 25,
+                    tier: 1
+                },
+                "bonzo_mask": {
+                    name: "Bonzo's Mask",
+                    required: 50,
+                    tier: 2
+                }
+            }
+        }
+    }
+};
