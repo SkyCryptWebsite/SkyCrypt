8dc8070e9292f434f4f9370b243a43684622b499
MartinNemi03 <mrmartinek@email.cz>
2020-08-05 04:39:01 +0200

    Added more constants

--- src/constants/dungeons.js
+++ src/constants/dungeons.js
@@ -2,7 +2,8 @@ module.exports = {
     boss_collections: {
         "bonzo": {
             name: "Bonzo",
-            texture: "",
+            texture: "12716ecbf5b8da00b05f316ec6af61e8bd02805b21eb8e440151468dc656549c",
+            max_tiers: 6,
             rewards: {
                 "red_nose": {
                     name: "Red Nose",
@@ -13,8 +14,46 @@ module.exports = {
                     name: "Bonzo's Mask",
                     required: 50,
                     tier: 2
+                },
+                "gold_bonzo_head": {
+                    name: "Golden Bonzo Head",
+                    required: 100,
+                    tier: 3
+                },
+                "bonzo_staff": {
+                    name: "Bonzo's Staff",
+                    required: 150,
+                    tier: 4
+                },
+                "recombobulator": {
+                    name: "Recombobulator 3000",
+                    required: 250,
+                    tier: 5
+                },
+                "diamond_bonzo_head": {
+                    name: "Diamond Bonzo Head",
+                    required: 1000,
+                    tier: 6
                 }
             }
+        },
+        "scarf": {
+            name: "Scarf",
+            texture: "7de7bbbdf22bfe17980d4e20687e386f11d59ee1db6f8b4762391b79a5ac532d",
+            max_tiers: 6,
+            rewards: {}
+        },
+        "professor": {
+            name: "The Professor",
+            texture: "9971cee8b833a62fc2a612f3503437fdf93cad692d216b8cf90bbb0538c47dd8",
+            max_tiers: 6,
+            rewards: {}
+        },
+        "thorn": {
+            name: "Thorn",
+            texture: "8b6a72138d69fbbd2fea3fa251cabd87152e4f1c97e5f986bf685571db3cc0",
+            max_tiers: 6,
+            rewards: {}
         }
     }
 };
