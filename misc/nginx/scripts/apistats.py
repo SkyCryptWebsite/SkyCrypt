b5b800d052510469a3b1d20bb127a38b7633fb38
Ty Chermsirivatana <professornitro@gmail.com>
2020-08-30 11:20:03 -0400

    Added lil note

--- misc/nginx/scripts/apistats.py
+++ misc/nginx/scripts/apistats.py
@@ -1,7 +1,8 @@
 import requests
 import json
-res = json.loads(requests.get("https://api.hypixel.net/key?key=).text)
+res = json.loads(requests.get("https://api.hypixel.net/key?key=1324402a-fbd1-4382-8008-411f8ecfad5f").text)
 print("The owner of the API key is {api_key}".format(api_key=res["record"]["owner"]))
 print("This key has been queried about {api_key} times".format(api_key=res["record"]["totalQueries"]))
 print("This key has a query limit of about {api_key}".format(api_key=res["record"]["limit"]))
 print("This key has been queried about {api_key} times in the past minute".format(api_key=res["record"]["queriesInPastMin"]))
+#Note to self, build API Key false statement
