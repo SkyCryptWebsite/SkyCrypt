e2b8f127848cbc374149f62dd25a20135d525934
metalcupcake5 <imcoolcom5@gmail.com>
2020-08-31 14:11:26 -0700

    Disable apiv2 for now

--- src/apiv2.js
+++ src/apiv2.js
@@ -14,7 +14,7 @@ function handleError(e, res) {
     });
 }
 
-module.exports = (app, db) => {
+/*module.exports = (app, db) => {
     const productInfo = {};
     const leaderboards = [];
 
@@ -375,3 +375,4 @@ module.exports = (app, db) => {
     });
 
 };
+*/
