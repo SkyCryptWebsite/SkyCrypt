9b1814f0cbbf361e32889318e47ca4d6fe280f59
boblovespi <boblovespi3.14@gmail.com>
2020-11-22 14:52:59 -0800

    nice one martin

--- src/constants/pets.js
+++ src/constants/pets.js
@@ -574,7 +574,7 @@ module.exports = {
             }
         },
         CROCHET_TIGER_PLUSHIE: {
-            description: `§7Increases §e${symbols.attack_speed} Bonus Attack Speed §7by §a40`,
+            description: `§7Increases §e${symbols.attack_speed} Bonus Attack Speed §7by §a35`,
             stats: {
                 bonus_attack_speed: 35
             }
