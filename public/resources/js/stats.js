9bb42bc083c98853701b69b3c4bed847e9b00819
Nate Stringham <nstringham@wccnet.edu>
2020-11-23 04:11:11 -0500

    refactor: :recycle: update renderLore to use css variables and be more readable

--- public/resources/js/stats.js
+++ public/resources/js/stats.js
@@ -157,61 +157,54 @@ document.addEventListener('DOMContentLoaded', function(){
         return false;
     };
 
-    function renderLore(text){
+    function renderLore(text) {
         let output = "";
-        let spansOpened = 0;
 
-        if (!text.startsWith("§")) {
-            text = `§7${text}`
-        }
+        color = null
+        formats = {}
+        const formatCodes = ['k', 'l', 'w', 'n', 'o']
 
-        const parts = text.split("§");
+        for (part of text.match(/(§[0-9a-f])*[^§]*/g)) {
 
-        if(parts.length == 1)
-            return text;
+            while (part.charAt(0) === '§') {
+                const code = part.charAt(1);
 
-        for(const part of parts){
-            const code = part.substring(0, 1);
-            const content = part.substring(1);
+                if (/[0-9a-f]/.test(code)) {
+                    color = code;
+                } else if (formatCodes.includes(code)){
+                    formats[code] = true;
+                } else if (code === 'r'){
+                    color = null;
+                    formats = {};
+                }
 
-            const format = constants.minecraft_formatting[code];
-
-            if(format === undefined)
-                continue;
-
-            if(format.type == 'color'){
-                for(; spansOpened > 0; spansOpened--)
-                    output += "</span>";
-
-                output += `<span style='${format.css}'>${content}`;
-
-                spansOpened++;
-            }else if(format.type == 'format'){
-                output += `<span style='${format.css}'>${content}`;
-
-                spansOpened++;
-            }else if(format.type == 'reset'){
-                for(; spansOpened > 0; spansOpened--)
-                    output += "</span>";
-
-                output += content;
+                part = part.substring(2);
             }
+
+            if (part.length === 0) continue;
+
+            classes = formatCodes.filter(key => formats[key]).map(x => '§' + x)
+
+            output += '<span';
+
+            if (color !== null) {
+                output += ` style='color: var(--§${color});'`;
+            }
+
+            if (classes.length > 0) {
+                output += ` class='${classes.join(', ')}'`;
+            }
+
+            output += `>${part}</span>`;
         }
-
-        for(; spansOpened > 0; spansOpened--)
-            output += "</span>";
-
-        const specialColor = constants.minecraft_formatting['6'];
 
         const matchingEnchants = constants.special_enchants.filter(a => output.includes(a));
 
-        for(const enchantment of matchingEnchants){
-            console.log(enchantment);
-            console.log(text)
-            if(enchantment == 'Power 6' || enchantment == 'Power 7' && text.startsWith("§8Breaking")){
+        for (const enchantment of matchingEnchants) {
+            if (enchantment == 'Power 6' || enchantment == 'Power 7' && text.startsWith("§8Breaking")) {
                 continue;
             }
-            output = output.replace(enchantment, `<span style='${specialColor.css}'>${enchantment}</span>`);
+            output = output.replace(enchantment, `<span style='color: var(--§6)'>${enchantment}</span>`);
         }
 
         return output;
