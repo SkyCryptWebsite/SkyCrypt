a4f03ce2c433ce3707e749ca91c76c20922946e2
Nate Stringham <nstringham@wccnet.edu>
2021-02-07 18:24:47 -0500

    style: whitespace

--- public/sw.js
+++ public/sw.js
@@ -16,7 +16,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-
     event.respondWith(
         fetch(event.request)
             .catch(() => {
