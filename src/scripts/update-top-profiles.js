c8b9bdbe28a03d6bb65db5ad35d636ca5e2eefb1
Nate Stringham <nstringham@wccnet.edu>
2021-03-12 16:34:56 -0500

    feat: :sparkles: give shiiyu host role and move it to the bottom so all the MAINTAINERs are together

--- src/scripts/update-top-profiles.js
+++ src/scripts/update-top-profiles.js
@@ -15,27 +15,27 @@ async function main(){
             position: 1,
             type: "MAINTAINER",
             message: "a dev or something idk"
-        }, 
-        Shiiyu: {
-            position: 2,
-            type: "MAINTAINER",
-            message: "<span class=\"stat-name\">Last online: </span><span class=\"stat-value\">January 1st, 1970</span>"
         },
         MartinNemi03: {
-            position: 3,
+            position: 2,
             type: "MAINTAINER",
             message: "\"lazy dev\" &nbsp; <b>(ﾉ´･ω･)ﾉ ﾐ ┸━┸</b>"
         },
         jjww2: {
-            position: 4,
+            position: 3,
             type: "MAINTAINER",
             message: "bob"
         },
         FantasmicGalaxy: {
-            position: 5,
+            position: 4,
             type: "MAINTAINER",
             message: "afk my melon minions :D"
-        }
+        },
+        Shiiyu: {
+            position: 5,
+            type: "HOST",
+            message: "<span class=\"stat-name\">Last online: </span><span class=\"stat-value\">January 1st, 1970</span>"
+        },
     };
 
     async function updateTopProfiles(){
