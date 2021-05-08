da844001221d197c6e9ddec88b1a9a1ddc627496
Davide Gaio <dggaio31@gmail.com>
2021-05-08 02:37:14 +0200

    Flying Pig

--- src/constants/pets.js
+++ src/constants/pets.js
@@ -978,5 +978,10 @@ module.exports = {
             tier: "LEGENDARY",
             description: `§7Gain §c8${symbols.ferocity} Ferocity §7for 5s on kill`
         },
+        PET_ITEM_FLYING_PIG: {
+            name: "Flying Pig",
+            tier: "UNCOMMON",
+            description: `§7Grants your pig pet the ability to fly while on your private island! You also don't need to hold a carrot on a stick to control your pig.`
+        },
     }
 }
