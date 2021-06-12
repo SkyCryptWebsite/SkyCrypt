b07192acc3bbd3a1b2c6fb548ed74aaa362ba470
MartinNemi03 <martinnemi03@pm.me>
2021-06-12 19:53:20 +0200

    format scripts with prettier

--- src/scripts/clear-favorite-cache.js
+++ src/scripts/clear-favorite-cache.js
@@ -1,24 +1,25 @@
-const cluster = require('cluster');
+const cluster = require("cluster");
 
-async function main(){
-    const { MongoClient } = require('mongodb');
+async function main() {
+  const { MongoClient } = require("mongodb");
 
-    const credentials = require('./../../credentials.json');
+  const credentials = require("./../../credentials.json");
 
-    const mongo = new MongoClient(credentials.dbUrl, { useUnifiedTopology: true });
-    await mongo.connect();
+  const mongo = new MongoClient(credentials.dbUrl, { useUnifiedTopology: true });
+  await mongo.connect();
 
-    const db = mongo.db(credentials.dbName);
+  const db = mongo.db(credentials.dbName);
 
-    async function clearFavoriteCache(){
-        // Clear cache for favorite
-        await db.collection('favoriteCache').deleteMany({});
+  async function clearFavoriteCache() {
+    // Clear cache for favorite
+    await db.collection("favoriteCache").deleteMany({});
 
-        setTimeout(clearFavoriteCache, 15 * 60 * 1000);
-    }
+    setTimeout(clearFavoriteCache, 15 * 60 * 1000);
+  }
 
-    clearFavoriteCache();
+  clearFavoriteCache();
 }
 
-if(cluster.isMaster)
-    main();
+if (cluster.isMaster) {
+  main();
+}
