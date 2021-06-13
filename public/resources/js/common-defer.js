ee74d5668725f3f950fecf5c4001af0ae9131d58
Nate Stringham <nstringham@wccnet.edu>
2021-06-13 06:57:58 -0400

    refactor: :recycle: seperate profile url validation into a funtion and use if instead of switch

--- public/resources/js/common-defer.js
+++ public/resources/js/common-defer.js
@@ -1,42 +1,61 @@
+/* global tippy:readonly */
+
+/**
+ * @param {string} url a url to be validated
+ *
+ * @throws {string} an error message
+ *
+ * @returns {string} a valid url
+ */
+function validateURL(url) {
+  const urlSegments = url.trim().split("/");
+  if (urlSegments.length < 1) {
+    throw "please enter a Minecraft username or UUID";
+  } else if (urlSegments.length > 2) {
+    throw `"${url}" has too many "/"`;
+  } else {
+    if (urlSegments.length === 2) {
+      if (urlSegments[1].match(/^[A-Za-z]+/)) {
+        urlSegments[1] = urlSegments[1].charAt(0).toUpperCase() + urlSegments[1].substr(1).toLowerCase();
+      } else if (!urlSegments[1].match(/^([0-9a-fA-F]{32})$/)) {
+        throw `"${urlSegments[1]}" is not a valid profile name or ID`;
+      }
+    }
+    if (
+      urlSegments[0].match(
+        /^([0-9a-fA-F]{8})-?([0-9a-fA-F]{4})-?([0-9a-fA-F]{4})-?([0-9a-fA-F]{4})-?([0-9a-fA-F]{12})$/
+      )
+    ) {
+      urlSegments[0] = urlSegments[0].replaceAll("-", "");
+    } else if (urlSegments[0].match(/^[\w ]{1,16}$/)) {
+      urlSegments[0] = urlSegments[0].replace(" ", "_");
+    } else {
+      throw `"${urlSegments[0]}" is not a valid username or UUID`;
+    }
+    return "/stats/" + urlSegments.join("/");
+  }
+}
+
 function handleSubmit(submitEvent) {
   submitEvent.preventDefault();
   const formData = new FormData(submitEvent.srcElement);
-  const urlSegments = formData.get("ign").trim().split("/");
-  let error;
-  switch (urlSegments.length) {
-    case 2:
-      if (!(urlSegments[1].match(/^([0-9a-fA-F]{32})$/) || urlSegments[1].match(/^[A-Z][a-z]+/))) {
-        error = `"${urlSegments[1]}" is not a valid profile name or ID`;
-        break;
-      }
-    case 1:
-      if (
-        urlSegments[0].match(
-          /^([0-9a-fA-F]{8})-?([0-9a-fA-F]{4})-?([0-9a-fA-F]{4})-?([0-9a-fA-F]{4})-?([0-9a-fA-F]{12})$/
-        )
-      ) {
-        urlSegments[0] = urlSegments[0].replace(/-/g, "");
-      } else if (urlSegments[0].match(/^[\w ]{1,16}$/)) {
-        urlSegments[0] = urlSegments[0].replace(/ /g, "_");
-      } else {
-        error = `"${urlSegments[0]}" is not a valid username or UUID`;
-        break;
-      }
-      window.location.href = "/stats/" + urlSegments.join("/");
-      return;
-  }
-  let errorTip = tippy(submitEvent.srcElement.querySelector("input"), {
-    trigger: "manual",
-    content: error || "please enter a valid Minecraft username or UUID",
-  });
-  errorTip.show();
-  setTimeout(() => {
-    errorTip.hide();
+  try {
+    window.location.href = validateURL(formData.get("ign"));
+  } catch (error) {
+    let errorTip = tippy(submitEvent.srcElement.querySelector("input"), {
+      trigger: "manual",
+      content: error || "please enter a valid Minecraft username or UUID",
+    });
+    errorTip.show();
     setTimeout(() => {
-      errorTip.destroy();
-    }, 500);
-  }, 1500);
+      errorTip.hide();
+      setTimeout(() => {
+        errorTip.destroy();
+      }, 500);
+    }, 1500);
+  }
 }
+
 document.querySelectorAll(".lookup-player").forEach((form) => {
   form.addEventListener("submit", handleSubmit);
 });
