bdcdb11884f4538941053b906e0160bec0e8b0a2
Nate Stringham <nstringham@wccnet.edu>
2021-06-17 05:52:04 -0400

    fix: :rotating_light: fix ts errors in localTimeElement

--- public/resources/ts/localTimeElement.ts
+++ public/resources/ts/localTimeElement.ts
@@ -1,16 +1,18 @@
 export class LocalTimeElement extends HTMLElement {
+  timeElement: HTMLTimeElement;
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     this.timeElement = document.createElement("time");
-    this.shadowRoot.appendChild(this.timeElement);
+    this.shadowRoot?.appendChild(this.timeElement);
   }
 
   static get observedAttributes() {
     return ["timestamp"];
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
+  attributeChangedCallback(name: string, oldValue: any, newValue: any) {
     if (name === "timestamp") {
       if (newValue != undefined) {
         if (!isNaN(newValue)) {
@@ -18,6 +20,7 @@ export class LocalTimeElement extends HTMLElement {
         }
         const date = new Date(newValue);
         this.timeElement.setAttribute("datetime", date.toISOString());
+        // @ts-ignore because typescript doesn't suport dateStyle and timeStyle yet https://github.com/microsoft/TypeScript/issues/44632
         this.timeElement.innerHTML = date.toLocaleString(undefined, { dateStyle: "long", timeStyle: "short" });
       } else {
         console.error("local-time must have a timestamp");
