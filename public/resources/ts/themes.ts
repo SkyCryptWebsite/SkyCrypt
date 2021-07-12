f2b6ab2db70dd9b33fd14a16c9ac6ea1a9a4f5d2
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2021-07-12 15:10:15 +0200

    Update themes.ts

--- public/resources/ts/themes.ts
+++ public/resources/ts/themes.ts
@@ -68,9 +68,6 @@ export function loadTheme(currentTheme: string): void {
           })
           .join(", ")})`;
         break;
-      case "linear-gradient":
-        value = 'linear-gradient(${background.angle} 0%, ${background.colors} ${background.end})';
-        break;
     }
     element.style.setProperty(`--${key}`, value);
   }
