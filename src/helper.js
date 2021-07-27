4d2b85a3108e292daeda4cb35e709e91bd1e7b33
dukio <dggaio31@gmail.com>
2021-07-27 20:47:45 +0200

    missing pets show at max level

--- src/helper.js
+++ src/helper.js
@@ -722,4 +722,16 @@ module.exports = {
       Math.floor(Math.random() * 9000 + 1000)
     );
   },
+
+  generateUUID: () => {
+    let u = "",
+      i = 0;
+    while (i++ < 36) {
+      let c = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"[i - 1],
+        r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      u += c == "-" || c == "4" ? c : v.toString(16);
+    }
+    return u;
+  },
 };
