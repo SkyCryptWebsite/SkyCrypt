6e36e97b135d56294245b01ac902943f3dfd57b4
Nate Stringham <nstringham@wccnet.edu>
2021-08-25 02:08:50 -0400

    fix: :goal_net: catch mongodb connection errors in init.js

--- init.js
+++ init.js
@@ -1,46 +1,52 @@
 import * as fsExtra from "fs-extra";
 import { item_tags } from "./src/constants.js";
-import { mongo, db } from "./src/mongo.js";
 
 fsExtra.ensureDirSync("cache");
 
-await db.collection("apiKeys").createIndex({ key: 1 }, { unique: true });
+try {
+  const { mongo, db } = await import("./src/mongo.js");
 
-await db.collection("profileStore").createIndex({ uuid: 1 }, { unique: true });
+  await db.collection("apiKeys").createIndex({ key: 1 }, { unique: true });
 
-await db.collection("profileStore").createIndex({ apis: 1 }, { partialFilterExpression: { apis: true } });
+  await db.collection("profileStore").createIndex({ uuid: 1 }, { unique: true });
 
-await db.collection("usernames").createIndex({ username: "text" });
+  await db.collection("profileStore").createIndex({ apis: 1 }, { partialFilterExpression: { apis: true } });
 
-await db.collection("usernames").createIndex({ uuid: 1 }, { unique: true });
+  await db.collection("usernames").createIndex({ username: "text" });
 
-await db.collection("favoriteCache").createIndex({ uuid: 1 }, { unique: true });
+  await db.collection("usernames").createIndex({ uuid: 1 }, { unique: true });
 
-await db.collection("members").createIndex({ uuid: 1, profile_id: 1 }, { unique: true });
+  await db.collection("favoriteCache").createIndex({ uuid: 1 }, { unique: true });
 
-await db.collection("guilds").createIndex({ gid: 1 }, { unique: true });
+  await db.collection("members").createIndex({ uuid: 1, profile_id: 1 }, { unique: true });
 
-await db.collection("guildMembers").createIndex({ uuid: 1 }, { unique: true });
+  await db.collection("guilds").createIndex({ gid: 1 }, { unique: true });
 
-await db.collection("guildMembers").createIndex({ gid: 1 });
+  await db.collection("guildMembers").createIndex({ uuid: 1 }, { unique: true });
 
-await db.collection("items").createIndex({ id: 1 }, { unique: true });
+  await db.collection("guildMembers").createIndex({ gid: 1 });
 
-await db.collection("items").createIndex({ name: "text", tag: "text" });
+  await db.collection("items").createIndex({ id: 1 }, { unique: true });
 
-for (const id in item_tags) {
-  await db.collection("items").updateOne({ id }, { $set: { tag: item_tags[id] } });
+  await db.collection("items").createIndex({ name: "text", tag: "text" });
+
+  for (const id in item_tags) {
+    await db.collection("items").updateOne({ id }, { $set: { tag: item_tags[id] } });
+  }
+
+  await db.collection("bazaar").createIndex({ productId: 1 }, { unique: true });
+
+  await db.collection("hypixelPlayers").createIndex({ uuid: 1 }, { unique: true });
+
+  await db.collection("profileCache").createIndex({ profile_id: 1 }, { unique: true });
+
+  await db.collection("topViews").createIndex({ total: -1 });
+
+  await db.collection("topViews").deleteMany({});
+
+  mongo.close();
+} catch (error) {
+  console.error(error);
 }
 
-await db.collection("bazaar").createIndex({ productId: 1 }, { unique: true });
-
-await db.collection("hypixelPlayers").createIndex({ uuid: 1 }, { unique: true });
-
-await db.collection("profileCache").createIndex({ profile_id: 1 }, { unique: true });
-
-await db.collection("topViews").createIndex({ total: -1 });
-
-await db.collection("topViews").deleteMany({});
-
-mongo.close();
 process.exit(0);
