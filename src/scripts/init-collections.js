d84810c19397356b7579b762bee0ef50a2bf3120
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-09-04 12:34:44 -0400

    refactor: :recycle: use Promise.all to initalize collections in parallel

--- src/scripts/init-collections.js
+++ src/scripts/init-collections.js
@@ -1,38 +1,40 @@
 import { db } from "../mongo.js";
 import { item_tags } from "../constants.js";
 
-await db.collection("apiKeys").createIndex({ key: 1 }, { unique: true });
+await Promise.all([
+  db.collection("apiKeys").createIndex({ key: 1 }, { unique: true }),
 
-await db.collection("profileStore").createIndex({ uuid: 1 }, { unique: true });
+  db.collection("profileStore").createIndex({ uuid: 1 }, { unique: true }),
 
-await db.collection("profileStore").createIndex({ apis: 1 }, { partialFilterExpression: { apis: true } });
+  db.collection("profileStore").createIndex({ apis: 1 }, { partialFilterExpression: { apis: true } }),
 
-await db.collection("usernames").createIndex({ username: "text" });
+  db.collection("usernames").createIndex({ username: "text" }),
 
-await db.collection("usernames").createIndex({ uuid: 1 }, { unique: true });
+  db.collection("usernames").createIndex({ uuid: 1 }, { unique: true }),
 
-await db.collection("favoriteCache").createIndex({ uuid: 1 }, { unique: true });
+  db.collection("favoriteCache").createIndex({ uuid: 1 }, { unique: true }),
 
-await db.collection("members").createIndex({ uuid: 1, profile_id: 1 }, { unique: true });
+  db.collection("members").createIndex({ uuid: 1, profile_id: 1 }, { unique: true }),
 
-await db.collection("guilds").createIndex({ gid: 1 }, { unique: true });
+  db.collection("guilds").createIndex({ gid: 1 }, { unique: true }),
 
-await db.collection("guildMembers").createIndex({ uuid: 1 }, { unique: true });
+  db.collection("guildMembers").createIndex({ uuid: 1 }, { unique: true }),
 
-await db.collection("guildMembers").createIndex({ gid: 1 });
+  db.collection("guildMembers").createIndex({ gid: 1 }),
 
-await db.collection("items").createIndex({ id: 1 }, { unique: true });
+  db.collection("items").createIndex({ id: 1 }, { unique: true }),
 
-await db.collection("items").createIndex({ name: "text", tag: "text" });
+  db.collection("items").createIndex({ name: "text", tag: "text" }),
 
-await Promise.all(
-  Object.entries(item_tags).map(([id, item]) => db.collection("items").updateOne({ id }, { $set: { tag: item } }))
-);
+  Promise.all(
+    Object.entries(item_tags).map(([id, item]) => db.collection("items").updateOne({ id }, { $set: { tag: item } }))
+  ),
 
-await db.collection("bazaar").createIndex({ productId: 1 }, { unique: true });
+  db.collection("bazaar").createIndex({ productId: 1 }, { unique: true }),
 
-await db.collection("hypixelPlayers").createIndex({ uuid: 1 }, { unique: true });
+  db.collection("hypixelPlayers").createIndex({ uuid: 1 }, { unique: true }),
 
-await db.collection("profileCache").createIndex({ profile_id: 1 }, { unique: true });
+  db.collection("profileCache").createIndex({ profile_id: 1 }, { unique: true }),
 
-await db.collection("featuredProfiles").createIndex({ total: -1 });
+  db.collection("featuredProfiles").createIndex({ total: -1 }),
+]);
