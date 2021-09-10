b39a8d9cc1d5b005e8e6b7a38ac8cd0794c1d9fe
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-09-10 11:49:34 -0400

    feat: :sparkles: add divine between mythic and supreme with color §4

--- src/constants/misc.js
+++ src/constants/misc.js
@@ -20,7 +20,7 @@ export const item_types = [
   "boots",
 ];
 
-/** @typedef {"common"|"uncommon"|"rare"|"epic"|"legendary"|"mythic"|"supreme"|"special"|"very special"} Rarity */
+/** @typedef {"common"|"uncommon"|"rare"|"epic"|"legendary"|"mythic"|"divine"|"supreme"|"special"|"very special"} Rarity */
 
 /** @type {Rarity[]} */
 export const rarities = [
@@ -30,6 +30,7 @@ export const rarities = [
   "epic",
   "legendary",
   "mythic",
+  "divine",
   "supreme",
   "special",
   "very special",
@@ -45,6 +46,7 @@ export const rarityColors = {
   epic: "5",
   legendary: "6",
   mythic: "d",
+  divine: "4",
   supreme: "4",
   special: "c",
   "very special": "c",
