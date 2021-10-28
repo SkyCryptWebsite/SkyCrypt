251ecbd07e0de2c2e5d6814d390ac5b42826914b
metalcupcake5 <imcoolcom5@gmail.com>
2021-10-28 18:53:20 +0000

    style(weight/lilyWeight.js): lint lilyWeight.js

--- src/weight/lilyWeight.js
+++ src/weight/lilyWeight.js
@@ -2,7 +2,6 @@ import credentials from "../credentials.js";
 import lilyWeight from "lilyweight";
 const lily = lilyWeight(credentials.hypixel_api_key);
 
-
 export async function calculateLilyWeight(uuid) {
   const weight = await lily.getWeight(uuid);
   return weight;
