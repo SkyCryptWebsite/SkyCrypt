e7fe100e8644ccc1b2d27e76e5c0ac47dda820cd
dukio <dggaio31@gmail.com>
2022-06-12 01:11:19 +0200

    potionColors in constants

--- src/lib.js
+++ src/lib.js
@@ -24,24 +24,6 @@ const Hypixel = axios.create({
   baseURL: "https://api.hypixel.net/",
 });
 const parseNbt = util.promisify(nbt.parse);
-const potionColors = {
-  0: "375cc4", // None
-  1: "cb5ba9", // Regeneration
-  2: "420a09", // Speed
-  3: "e19839", // Poison
-  4: "4d9130", // Fire Resistance
-  5: "f52423", // Instant Health
-  6: "1f1f9e", // Night Vision
-  7: "22fc4b", // Jump Boost
-  8: "474c47", // Weakness
-  9: "912423", // Strength
-  10: "5c6e83", // Slowness
-  11: "f500f5", // Uncraftable
-  12: "420a09", // Instant Damage
-  13: "2f549c", // Water Breathing
-  14: "818595", // Invisibility
-  15: "f500f5", // Uncraftable
-};
 
 function getMinMax(profiles, min, ...path) {
   let output = null;
@@ -456,7 +438,7 @@ async function processItems(base64, customTextures = false, packs, cacheOnly = f
 
     // Set custom texture for colored potions
     if (item.id == 373) {
-      const color = potionColors[item.Damage % 16];
+      const color = constants.potionColors[item.Damage % 16];
 
       const type = item.Damage & 16384 ? "splash" : "normal";
 
