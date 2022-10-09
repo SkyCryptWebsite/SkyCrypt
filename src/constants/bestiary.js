2ad4179bc1861ab2ec7d21a557ca4c82e1e63605
DuckySoLucky <robertkovac160@gmail.com>
2022-10-09 20:44:33 +0200

    merge with dev

---
+++ src/constants/bestiary.js
@@ -0,0 +1,597 @@
+export const BESTIARY = {
+    private: {
+      max: 5,
+      mobs: [
+        {
+          id: "kills_family_zombie",
+          name: "Zombie",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+          maxTier: 5,
+        },
+        {
+          id: "kills_family_skeleton",
+          name: "Skeleton",
+          skyblockId: "SKELETON_SKULL",
+          itemId: 397,
+          damage: 0,
+          maxTier: 5,
+        },
+        {
+          id: "kills_family_spider",
+          name: "Spider",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+          maxTier: 5,
+        },
+        {
+          id: "kills_family_cave_spider",
+          name: "Cave Spider",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+          maxTier: 5,
+        },
+        {
+          id: "kills_family_enderman_private",
+          name: "Enderman",
+          head: "96c0b36d53fff69a49c7d6f3932f2b0fe948e032226d5e8045ec58408a36e951",
+          maxTier: 5,
+        },
+        {
+          id: "kills_family_slime",
+          name: "Slime",
+          head: "895aeec6b842ada8669f846d65bc49762597824ab944f22f45bf3bbb941abe6c",
+          maxTier: 5,
+        },
+        {
+          id: "kills_family_witch",
+          name: "Witch",
+          head: "fce6604157fc4ab5591e4bcf507a749918ee9c41e357d47376e0ee7342074c90",
+          maxTier: 5,
+        },
+      ],
+    },
+    hub: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_zombie_villager",
+          name: "Zombie Villager",
+          head: "e5e08a8776c1764c3fe6a6ddd412dfcb87f41331dad479ac96c21df4bf3ac89c",
+        },
+        {
+          id: "kills_family_unburried_zombie",
+          name: "Crypt Ghoul",
+          skyblockId: "GOLD_SWORD",
+          itemId: 283,
+          damage: 0,
+        },
+        {
+          id: "kills_family_ruin_wolf",
+          name: "Wolf",
+          head: "69d1d3113ec43ac2961dd59f28175fb4718873c6c448dfca8722317d67",
+        },
+        {
+          id: "kills_family_old_wolf",
+          name: "Old Wolf",
+          head: "d359537c15534f61c1cd886bc118774ed22280e7cdab6613870160aad4ca39",
+        },
+      ],
+    },
+    spiders: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_dasher_spider",
+          name: "Dasher Spider",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+        },
+        {
+          id: "kills_family_spider_jockey",
+          name: "Spider Jockey",
+          head: "70939373cafe4b1f5397aafd09f3bb1663e7b629a41a75fbdc1860b6bf8b475f",
+        },
+        {
+          id: "kills_family_weaver_spider",
+          name: "Weaver Spider",
+          head: "921438f646dc04515967e8195ccc3d31e23b02f9aa0ac914edc2822f98394b86",
+        },
+        {
+          id: "kills_family_voracious_spider",
+          name: "Voracious Spider",
+          head: "8300986ed0a04ea79904f6ae53f49ed3a0ff5b1df62bba622ecbd3777f156df8",
+        },
+        {
+          id: "kills_family_splitter_spider",
+          name: "Splitter Spider",
+          head: "2acf69fc7af5497a1741891d1e5bf32b96ae0d6c0bbd3c14758ea44a03c57283",
+        },
+        {
+          id: "kills_family_brood_mother_spider",
+          boss: true,
+          name: "Brood Mother",
+          head: "cf0622b3998d42b34d5bc760bb2c83fdbc6e68fab05b7ea17b35097ed81190d6",
+        },
+        {
+          id: "kills_family_respawning_skeleton",
+          name: "Gravel Skeleton",
+          skyblockId: "SKELETON_SKULL",
+          itemId: 397,
+          damage: 0,
+        },
+        {
+          id: "kills_family_random_slime",
+          name: "Rain Slime",
+          head: "895aeec6b842ada8669f846d65bc49762597824ab944f22f45bf3bbb941abe6c",
+        },
+        {
+          id: "kills_family_arachne_keeper",
+          name: "Arachne's Keeper",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+        },
+        {
+          id: "kills_family_arachne_brood",
+          name: "Arachne's Brood",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+        },
+        {
+          boss: true,
+          id: "kills_family_arachne",
+          name: "Arachne",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+        },
+      ],
+    },
+    fortress: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_ashfang",
+          boss: true,
+          name: "Ashfang",
+          head: "3d2a5b4b109bd788edba0171d0aab8a55305ac2f56184df70a319cd488a36c3e",
+        },
+        {
+          id: "kills_family_barbarian_duke_x",
+          boss: true,
+          name: "Barbarian Duke X",
+          head: "fee9fc7c181e2f630f6b1af85d4951359cafcfa82ff5ea3bc283e0e6088c6e54",
+        },
+        {
+          id: "kills_family_bladesoul",
+          boss: true,
+          name: "Bladesoul",
+          head: "7f5f3384384d7f03bf97a73099b0bafb72c538fc0415ac8613b664f87579a379",
+        },
+        {
+          id: "kills_family_blaze",
+          name: "Blaze",
+          head: "b78ef2e4cf2c41a2d14bfde9caff10219f5b1bf5b35a49eb51c6467882cb5f0",
+        },
+        {
+          id: "kills_family_flaming_spider",
+          name: "Flaming Spider",
+          head: "5a65ef23ea35047150d33880473a47fe635f0c0e3c823bd76dc849cb02441655",
+        },
+        {
+          id: "kills_family_ghast",
+          name: "Ghast",
+          head: "de8a38e9afbd3da10d19b577c55c7bfd6b4f2e407e44d4017b23be9167abff02",
+        },
+        {
+          id: "kills_family_mage_outlaw",
+          boss: true,
+          name: "Mage Outlaw",
+          head: "ebec99b3a050fd277528407676366e20b92006ad88d14727a4d99eb8cb273b62",
+        },
+        {
+          id: "kills_family_magma_cube",
+          name: "Magma Cube",
+          head: "38957d5023c937c4c41aa2412d43410bda23cf79a9f6ab36b76fef2d7c429",
+        },
+        {
+          id: "kills_family_magma_cube_boss",
+          boss: true,
+          name: "Magma Cube Boss",
+          head: "38957d5023c937c4c41aa2412d43410bda23cf79a9f6ab36b76fef2d7c429",
+        },
+        {
+          id: "kills_family_matcho",
+          name: "Matcho",
+          head: "ef2daabb78a1f7aa12d145d88c0ca46b9e856f5534e9286e555faf0c291f4fd5",
+        },
+        {
+          id: "kills_family_charging_mushroom_cow",
+          name: "Mushroom Bull",
+          head: "2b52841f2fd589e0bc84cbabf9e1c27cb70cac98f8d6b3dd065e55a4dcb70d77",
+        },
+        {
+          id: "kills_family_pigman",
+          name: "Pigman",
+          head: "74e9c6e98582ffd8ff8feb3322cd1849c43fb16b158abb11ca7b42eda7743eb",
+        },
+        {
+          id: "kills_family_wither_skeleton",
+          name: "Wither Skeleton",
+          head: "7953b6c68448e7e6b6bf8fb273d7203acd8e1be19e81481ead51f45de59a8",
+        },
+        {
+          id: "kills_family_wither_spectre",
+          name: "Wither Spectre",
+          head: "f6aa5acfd973f67ba8e249ae07a11ae7ca713c900d90d09ce0903c79ddd7dd9e",
+        },
+      ],
+    },
+    end: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_enderman",
+          name: "Enderman",
+          head: "96c0b36d53fff69a49c7d6f3932f2b0fe948e032226d5e8045ec58408a36e951",
+        },
+        {
+          id: "kills_family_obsidian_wither",
+          name: "Obsidian Defender",
+          head: "7953b6c68448e7e6b6bf8fb273d7203acd8e1be19e81481ead51f45de59a8",
+        },
+        {
+          id: "kills_family_watcher",
+          name: "Watcher",
+          head: "daa8fc8de6417b48d48c80b443cf5326e3d9da4dbe9b25fcd49549d96168fc0",
+        },
+        {
+          id: "kills_family_zealot_enderman",
+          name: "Zealot",
+          skyblockId: "SUMMONING_EYE",
+          itemId: 130,
+          damage: 0,
+        },
+        {
+          id: "kills_family_endermite",
+          name: "Endermite",
+          head: "5bc7b9d36fb92b6bf292be73d32c6c5b0ecc25b44323a541fae1f1e67e393a3e",
+        },
+        {
+          id: "kills_family_dragon",
+          boss: true,
+          name: "Dragon",
+          skyblockId: "DRAGON_EGG",
+          itemId: 122,
+          damage: 0,
+        },
+        {
+          id: "kills_family_corrupted_protector",
+          boss: true,
+          name: "Endstone Protector",
+          head: "22bcaceeb4162f400d44743315932ac820d3119ac8986a0161a726161ccc93fc",
+        },
+        {
+          id: "kills_family_voidling_fanatic",
+          name: "Voidling Fanatic",
+          head: "e53b7bcd56f0b95f87dd79ed17636beb9d8346744014228ea2f6b110b148c1",
+        },
+        {
+          id: "kills_family_voidling_extremist",
+          name: "Voidling Extremist",
+          head: "eb07594e2df273921a77c101d0bfdfa1115abed5b9b2029eb496ceba9bdbb4b3",
+        },
+      ],
+    },
+    caverns: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_invisible_creeper",
+          name: "Sneaky Creeper",
+          skyblockId: "CREEPER_SKULL",
+          itemId: 397,
+          damage: 4,
+        },
+        {
+          id: "kills_family_lapis_zombie",
+          name: "Lapis Zombie",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 351,
+          damage: 4,
+        },
+        {
+          id: "kills_family_redstone_pigman",
+          name: "Redstone Pigman",
+          skyblockId: "REDSTONE",
+          itemId: 331,
+          damage: 0,
+        },
+        {
+          id: "kills_family_emerald_slime",
+          name: "Emerald Slime",
+          head: "5774e86aa4cff792395b77add57b00bb1a102f8f0f0980f4d55b3d7aff1e6da9",
+        },
+        {
+          id: "kills_family_diamond_zombie",
+          name: "Miner Zombie",
+          head: "d28d9ff54188a1afee65b94f3bfcce321c643a34590c14b192b2e3ec2f525d3",
+        },
+        {
+          id: "kills_family_diamond_skeleton",
+          name: "Miner Skeleton",
+          head: "836bbc4215cea1b6a484e893b116e73459ef36bffc622741e577e9493a41e6e",
+        },
+        {
+          id: "kills_family_caverns_ghost",
+          name: "Ghost",
+          head: "981977918a1180e0dec879e6bcd1aa39494977bb913be2ab01aff1db1fa4",
+        },
+        {
+          id: "kills_family_goblin",
+          name: "Goblin",
+          head: "b6b972e32d761b192626e5d6d01edc094940910103cea5e2e2d1f231adb755d5",
+        },
+        {
+          id: "kills_family_treasure_hoarder",
+          name: "Treasure Hoarder",
+          head: "b2b12a814ced8af02cddf29a37e7f3011e430e8a18b38b706f27c6bd31650b65",
+        },
+        {
+          id: "kills_family_ice_walker",
+          name: "Ice Walker",
+          skyblockId: "PACKED_ICE",
+          itemId: 174,
+          damage: 0,
+        },
+        {
+          id: "kills_family_automaton",
+          name: "Automaton",
+          head: "22bcaceeb4162f400d44743315932ac820d3119ac8986a0161a726161ccc93fc",
+        },
+        {
+          id: "kills_family_butterfly",
+          name: "Butterfly",
+          head: "9fd806defdfdf59b1f2609c8ee364666de66127a623415b5430c9358c601ef7c",
+        },
+        {
+          id: "kills_family_team_treasurite",
+          name: "Grunt",
+          head: "ef5e03b8de119f8589a808224cbea737f5dcf24235799b7378ac5bf06abf6dd4",
+        },
+        {
+          id: "kills_family_sludge",
+          name: "Sludge",
+          head: "895aeec6b842ada8669f846d65bc49762597824ab944f22f45bf3bbb941abe6c",
+        },
+        {
+          id: "kills_family_thyst",
+          name: "Thyst",
+          head: "5bc7b9d36fb92b6bf292be73d32c6c5b0ecc25b44323a541fae1f1e67e393a3e",
+        },
+        {
+          id: "kills_family_worms",
+          name: "Worm",
+          head: "df03ad96092f3f789902436709cdf69de6b727c121b3c2daef9ffa1ccaed186c",
+        },
+        {
+          id: "kills_family_yog",
+          name: "Yog",
+          head: "38957d5023c937c4c41aa2412d43410bda23cf79a9f6ab36b76fef2d7c429",
+        },
+      ],
+    },
+    park: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_pack_spirit",
+          name: "Pack Spirit",
+          head: "b7c8bef6beb77e29af8627ecdc38d86aa2fea7ccd163dc73c00f9f258f9a1457",
+        },
+        {
+          id: "kills_family_howling_spirit",
+          name: "Howling Spirit",
+          head: "b7c8bef6beb77e29af8627ecdc38d86aa2fea7ccd163dc73c00f9f258f9a1457",
+        },
+        {
+          id: "kills_family_soul_of_the_alpha",
+          name: "Soul of the Alpha",
+          head: "b7c8bef6beb77e29af8627ecdc38d86aa2fea7ccd163dc73c00f9f258f9a1457",
+        },
+      ],
+    },
+    spooky: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_trick_or_treater",
+          name: "Trick or Treater",
+          head: "d7cbe061b445b88cb2df59accc8d2c1c12118fe0b2127e6e75813550adac67cf",
+        },
+        {
+          id: "kills_family_wither_gourd",
+          name: "Wither Gourd",
+          head: "68f2ffc6fb4e9959b9a7a317f51a6775a159ddc2241dbd6c774d3ac08b6",
+        },
+        {
+          id: "kills_family_phantom_spirit",
+          name: "Phantom Spirit",
+          head: "653b2f7c5e177bd67cead30d0ee53425c7684c975c8c2a5273a09ca49a1f6cdd",
+        },
+        {
+          id: "kills_family_scary_jerry",
+          name: "Scary Jerry",
+          head: "df202dc24d15f7ce36e02fb4b3e8153b146ab9c1724aaa5d484c741da0eeb6fd",
+        },
+        {
+          id: "kills_family_wraith",
+          name: "Wraith",
+          head: "aeabc3075cf41f38e6df1236b95caafcba1bee22c49d804b9427488c2f6e0efc",
+        },
+        {
+          id: "kills_family_batty_witch",
+          name: "Crazy Witch",
+          head: "fce6604157fc4ab5591e4bcf507a749918ee9c41e357d47376e0ee7342074c90",
+        },
+        {
+          id: "kills_family_headless_horseman",
+          boss: true,
+          name: "Headless Horseman",
+          head: "4c6570f1242992f6eba23ee582598c39e3e745383273deef8b3977583fe3cf5",
+        },
+      ],
+    },
+    catacombs: {
+      max: 1000,
+      mobs: [
+        {
+          id: "kills_family_lost_adventurer",
+          name: "Lost Adventurer",
+          head: "1e0329f42192ee7c1a0c704f82dbbbe7c00fbfa3f020c1067a208630b991b988",
+        },
+        {
+          id: "kills_family_zombie_grunt",
+          name: "Zombie Grunt",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+        },
+        {
+          id: "kills_family_zombie_soldier",
+          name: "Zombie Soldier",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+        },
+        {
+          id: "kills_family_zombie_knight",
+          name: "Zombie Soldier",
+          head: "25d2f31ba162fe6272e831aed17f53213db6fa1c4cbe4fc827f3963cc98b9",
+        },
+        {
+          id: "kills_family_skeleton_grunt",
+          name: "Skeleton Grunt",
+          head: "70471677b37ae842c2bd232e16eeb84d5a493231eecec072da38be3127dd5c8",
+        },
+        {
+          id: "kills_family_skeleton_soldier",
+          name: "Skeleton Soldier",
+          head: "219688e0c2f05aeb979d6a1b8c911957b7d3657e14b57af93c5ef6f6a59569dd",
+        },
+        {
+          id: "kills_family_skeleton_master",
+          name: "Skeleton Master",
+          head: "74e95e1b7dc82ba7845a68fc6a312f4cd90ee2f6cce26a68c88b04b1072d879",
+        },
+        {
+          id: "kills_family_scared_skeleton",
+          name: "Scared Skeleton",
+          skyblockId: "SKELETON_SKULL",
+          itemId: 397,
+          damage: 0,
+        },
+        {
+          id: "kills_family_diamond_guy",
+          name: "Angry Archeologist",
+          head: "c48c783458e4cf8518e8ab5863fbc4cb948f90568eeb9a60d16c4fde2b96c033",
+        },
+        {
+          id: "kills_family_dungeon_respawning_skeleton",
+          name: "Undead Skeleton",
+          skyblockId: "SKELETON_SKULL",
+          itemId: 397,
+          damage: 0,
+        },
+        {
+          id: "kills_family_skeletor",
+          name: "Skeletor",
+          head: "89d074ad9b9971879eb325bddff3675f7224856bd6d569fc8d483c133d73005d",
+        },
+        {
+          id: "kills_family_sniper_skeleton",
+          name: "Sniper",
+          head: "b18c071f080dba50a62a6263ff724edc157ce4fb4883cceff2491d5bbde830c1",
+        },
+        {
+          id: "kills_family_crypt_souleater",
+          name: "Crypt Souleater",
+          head: "66f349270a3b851896cdad842f5eec6e140bd9119b75c074955c3be7865e27c7",
+        },
+        {
+          id: "kills_family_crypt_dreadlord",
+          name: "Crypt Dreadlord",
+          head: "66f349270a3b851896cdad842f5eec6e140bd9119b75c074955c3be7865e27c7",
+        },
+        {
+          id: "kills_family_crypt_tank_zombie",
+          name: "Tank Zombie",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+        },
+        {
+          id: "kills_family_super_tank_zombie",
+          name: "Super Tank Zombie",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+        },
+        {
+          id: "kills_family_crypt_lurker",
+          name: "Crypt Lurker",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+        },
+        {
+          id: "kills_family_cellar_spider",
+          name: "Cellar Spider",
+          head: "41645dfd77d09923107b3496e94eeb5c30329f97efc96ed76e226e98224",
+        },
+        {
+          id: "kills_family_watcher_summon_undead",
+          name: "Undead",
+          head: "f4624a9a8c69ca204504abb043d47456cd9b09749a36357462303f276a229d4",
+        },
+        {
+          id: "kills_family_lonely_spider",
+          name: "Lonely Spider",
+          head: "cd541541daaff50896cd258bdbdd4cf80c3ba816735726078bfe393927e57f1",
+        },
+        {
+          id: "kills_family_shadow_assassin",
+          name: "Shadow Assassin",
+          head: "3399e00f404411e465d74388df132d51fe868ecf86f1c073faffa1d9172ec0f3",
+        },
+        {
+          id: "kills_family_king_midas",
+          name: "King Midas",
+          head: "62bca085750043503f5df9f7def824a2e3acfc27842bcd09d2b6695881e832f5",
+        },
+        {
+          id: "kills_family_super_archer",
+          name: "Super Archer",
+          head: "dcae6db0b59a64305076d966d8e7b9a97be46dae3a807714fa86d37848f6",
+        },
+        {
+          id: "kills_family_crypt_witherskeleton",
+          name: "Withermancer",
+          head: "7953b6c68448e7e6b6bf8fb273d7203acd8e1be19e81481ead51f45de59a8",
+        },
+        {
+          id: "kills_family_zombie_commander",
+          name: "Zombie Commander",
+          skyblockId: "ZOMBIE_SKULL",
+          itemId: 397,
+          damage: 2,
+        },
+      ],
+    },
+  };
+  
+  export const BEASTIARY_KILLS = {
+    regular: [
+      10, 25, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 200000, 300000, 400000, 500000, 600000,
+      700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000, 1600000, 1700000, 1800000, 1900000,
+      2000000, 2100000, 2200000, 2300000, 2400000, 2500000, 2600000, 2700000, 2800000, 2900000, 3000000,
+    ],
+    boss: [
+      2, 5, 10, 20, 30, 40, 50, 75, 100, 150, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500,
+      1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200,
+    ],
+  };
