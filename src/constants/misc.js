e441878686c1dbeb28f5ba2f191ced849a8bce90
DuckySoLucky <robertkovac160@gmail.com>
2022-12-27 21:13:45 +0100

    feat: add perks (#1787)
    * feat: add perks
    
    * feat: add ice essence
    
    * style: `npm run prettier:fix`
    
    * feat: add Crimson Essence
    
    * Update misc.js
    
    * Update misc.js

--- src/constants/misc.js
+++ src/constants/misc.js
@@ -451,3 +451,216 @@ export const DOJO = {
     damage: 0,
   },
 };
+
+export const ESSENCE_SHOP = {
+  undead: {
+    catacombs_boss_luck: {
+      name: "Boss Luck",
+      description: "Increases the quality of boss rewards in The Catacombs by 1-10%.",
+      maxLevel: 4,
+    },
+    catacombs_looting: {
+      name: "Looting",
+      description: "Increases the quality of mob loot drops in The Catacombs by 1-10%.",
+      maxLevel: 5,
+    },
+    revive_stone: {
+      name: "Help of the Fairies",
+      description: "Start every run in The Catacombs with an invisible Revive Stone.",
+      maxLevel: 1,
+    },
+    catacombs_health: {
+      name: "Health Essence",
+      description: "Increases your base Health while in The Catacombs by 25-125.",
+      maxLevel: 5,
+    },
+    catacombs_defense: {
+      name: "Defense Essence",
+      description: "Increases your base Defense while in The Catacombs by 10-50.",
+      maxLevel: 5,
+    },
+    catacombs_strength: {
+      name: "Strength Essence",
+      description: "Increases your base Strength while in The Catacombs by 10-50.",
+      maxLevel: 5,
+    },
+    catacombs_intelligence: {
+      name: "Intelligence Essence",
+      description: "Increases your base Intelligence while in The Catacombs by 15-75.",
+      maxLevel: 5,
+    },
+    catacombs_crit_damage: {
+      name: "Critical Damage Essence",
+      description: "Increases your base Critical Damage while in The Catacombs by 10-50.",
+      maxLevel: 5,
+    },
+  },
+  wither: {
+    permanent_health: {
+      name: "Forbidden Health",
+      description: "Increases your Health by 2-10.",
+      maxLevel: 5,
+      bonus: 2,
+    },
+    permanent_defense: {
+      name: "Forbidden Defense",
+      description: "Increases your Defense by 1-5.",
+      maxLevel: 5,
+      bonus: 1,
+    },
+    permanent_speed: {
+      name: "Forbidden Speed",
+      description: "Increases your Speed by 1-2.",
+      maxLevel: 2,
+      bonus: 1,
+    },
+    permanent_intelligence: {
+      name: "Forbidden Intelligence",
+      description: "Increases your Intelligence by 2-10.",
+      maxLevel: 5,
+      bonus: 2,
+    },
+    permanent_strength: {
+      name: "Forbidden Strength",
+      description: "Increases your Strength by 1-5.",
+      maxLevel: 5,
+      bonus: 1,
+    },
+    forbidden_blessing: {
+      name: "Forbidden Blessing",
+      description: "Blessings are 1-10% more effective on you.",
+      maxLevel: 10,
+    },
+  },
+  dragon: {
+    inc_zealots_odds: {
+      name: "Zealuck",
+      description: "Increases the chance to find a special Zealot by 2-10%.",
+      maxLevel: 5,
+    },
+    combat_wisdom_in_end: {
+      name: "Ender Training",
+      description: "Gain + 3 - 7 Combat Wisdom while in The End.",
+      maxLevel: 2,
+    },
+    edrag_cd: {
+      name: "Infused Dragon",
+      description: "Increases the Crit Damage of your Ender Dragon Pet by + 2 - 10 CD.",
+      maxLevel: 5,
+    },
+    dragon_reforges_buff: {
+      name: "Two-Headed Strike",
+      description: "Renowed and Spiked Reforges apply an extra 2-10 Bonus Attack Speed on your gear.",
+      maxLevel: 4,
+    },
+    fero_vs_dragons: {
+      name: "Rageborn",
+      description: "Gain +2-10 Ferocity against Dragons.",
+      maxLevel: 5,
+    },
+    mana_after_ender_kill: {
+      name: "Recharge",
+      description: "Regain 2-20 Mana after killing an Enderman or Endermite.",
+      maxLevel: 10,
+    },
+    flat_damage_vs_ender: {
+      name: "One Punch",
+      description:
+        "After all other damage, add 500 - 2,500 damage to the first strike against Endermen and Endermites.",
+      maxLevel: 5,
+    },
+    increased_sup_chances: {
+      name: "Dragon Piper",
+      description:
+        "Gain +10% chance to spawn a Superior Dragon if you placed a Summoning Eye.\nThis perk only applies once per dragon across players.",
+      maxLevel: 1,
+    },
+  },
+  spider: {
+    empowered_agility: {
+      name: "Empowered Agility",
+      description: "Reduces the Mana cost of some movement abilities by 1-10%.",
+      maxLevel: 10,
+    },
+    bane: {
+      name: "Bane",
+      description: "Increases damage dealt to Spiders by 3-15%.",
+      maxLevel: 5,
+    },
+    vermin_control: {
+      name: "Vermin Control",
+      description: "Receive 3-15% less damage from Spiders.",
+      maxLevel: 5,
+    },
+    spider_training: {
+      name: "Spider Training",
+      description: "Increases your Combat Wisdom by 3-10 while on the Spider's Den.",
+      maxLevel: 3,
+    },
+  },
+  ice: {
+    cold_efficiency: {
+      name: "Cold Efficiency",
+      description: "Increases the Mage class experience by 2-10%.",
+      maxLevel: 5,
+    },
+    cooled_forges: {
+      name: "Cooled Forges",
+      description: "4-20% chance to get double essence when salvaging.",
+      maxLevel: 5,
+    },
+    frozen_skin: {
+      name: "Frozen Skin",
+      description: "Increases your Defense by 2-10 while on Jerry's Workshop.",
+      maxLevel: 5,
+    },
+    season_of_joy: {
+      name: "Season of Joy",
+      description: "Gain 1-10% extra Gifts from the Gift Attack event.",
+      maxLevel: 10,
+    },
+  },
+  crimson: {
+    strongarm_kuudra: {
+      name: "Strongarm",
+      description: "Your Fishing Rods pull Supplies and Fuel Cells 10-25% further during the Kuudra Boss Fight.",
+      maxLevel: 2,
+    },
+    fresh_tools_kuudra: {
+      name: "Fresh Tools",
+      description: "2-10% chance to double the speed of repairing Elle's Ballista for 5s during the Kuudra Boss Fight.",
+      maxLevel: 5,
+    },
+    headstart_kuudra: {
+      name: "Headstart",
+      description: "Start the Kuudra Boss Fight with 10-50 Tokens.",
+      maxLevel: 5,
+    },
+    master_kuudra: {
+      name: "Kuudra Master",
+      description:
+        "Deal 0.1% increased damage to Kuudra's Minions, multiplied by the highest defeated wave in that tier or higher. Max 10%.",
+      maxLevel: 1,
+    },
+    fungus_fortuna: {
+      name: "Fungus Fortuna",
+      description: "Gain 2-20 Mining Fortune when mining Mycelium.",
+      maxLevel: 10,
+    },
+    harena_fortuna: {
+      name: "Harena Fortuna",
+      description: "Gain 2-20 Mining Fortune when mining Red Sand.",
+      maxLevel: 10,
+    },
+    crimson_training: {
+      name: "Crimson Training",
+      description: "Increases your Combat Wisdom by 3-10 while on the Crimson Isle and in the Kuudra Boss Fight.",
+      maxLevel: 3,
+    },
+    wither_piper: {
+      name: "Wither Piper",
+      description: "2-10% increased chance to find Vanquishers on the Crimson Isle.",
+      maxLevel: 5,
+    },
+  },
+};
--- src/lib.js
+++ src/lib.js
@@ -2057,6 +2057,7 @@ export async function getStats(
 
   const misc = {};
 
+  output.perks = userProfile.perks || {};
   misc.milestones = {};
   misc.objectives = {};
   misc.races = {};
