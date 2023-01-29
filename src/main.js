8e413fe651ae012b07046fe1bd24ec0bdf39757a
WarpWing <professornitro@gmail.com>
2023-01-29 15:29:04 -0500

    My mental comes full circle

--- src/main.js
+++ src/main.js
@@ -1,30 +1,25 @@
 import cluster from "cluster";
-import os from "os";
 
-const initCluster = async () => {
-  if (cluster.isPrimary) {
-    await import("./master.js");
+const timeNow = Date.now();
 
-    const totalCpus = os.cpus().length;
-    const cpus = Math.min(process.env?.NODE_ENV === "development" ? 2 : 8, totalCpus);
+if (cluster.isPrimary) {
+  import("./master.js");
 
-    for (let i = 0; i < cpus; i++) {
-      cluster.fork();
-    }
+  const os = await import("os");
+  const totalCpus = os.cpus().length;
+  const cpus = Math.min(process.env?.NODE_ENV === "development" ? 2 : 8, totalCpus);
 
-    cluster.on("exit", (worker, code, signal) => {
-      console.log(
-        `Worker ${worker.id} died with code ${code} ${signal ? `and signal ${signal}` : ""} (pid:${worker.process.pid})`
-      );
-
-      const fw = cluster.fork();
-      console.log(`Worker respawned with id ${fw.id} (pid:${fw.process.pid})`);
-    });
-
-    console.log(`Running SkyBlock Stats on ${cpus} cores`);
-  } else {
-    await import("./app.js");
+  for (let i = 0; i < cpus; i += 1) {
+    cluster.fork();
   }
-};
 
-initCluster();
+  cluster.on("exit", () => {
+    console.log(`Worker died.`);
+    cluster.fork();
+  });
+
+  console.log(`Running SkyBlock Stats on ${cpus} cores`);
+  console.log("Started in " + (Date.now() - timeNow) + "ms");
+} else {
+  import("./app.js");
+}
