5db291edc2c992eb07aa5e890a4e5551bf6d1aa5
WarpWing <professornitro@gmail.com>
2023-01-29 15:04:58 -0500

    refactor: Optimized Cluster Code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import cluster from "cluster";
 
-if (cluster.isPrimary) {
-  await import("./master.js");
+const initCluster = async () => {
+  if (!cluster.isPrimary) {
+    return import("./app.js");
+  }
 
+  await import("./master.js");
   const os = await import("os");
 
   const totalCpus = os.cpus().length;
-  const cpus = Math.min(process.env?.NODE_ENV != "development" ? 8 : 2, totalCpus);
+  const cpus = Math.min(process.env?.NODE_ENV === "development" ? 2 : 8, totalCpus);
 
   for (let i = 0; i < cpus; i += 1) {
     cluster.fork();
@@ -22,6 +25,6 @@ if (cluster.isPrimary) {
   });
 
   console.log(`Running SkyBlock Stats on ${cpus} cores`);
-} else {
-  import("./app.js");
-}
+};
+
+initCluster();
