98c958fbffee015ab782a439586a2c80bc7b62c3
MartinNemi03 <martinnemi03@pm.me>
2023-02-12 01:42:07 +0100

    feat: split renderer endpoints into their own routers

---
+++ src/routes/item.js
@@ -0,0 +1,51 @@
+import cors from "cors";
+import express from "express";
+
+import { db } from "../mongo.js";
+
+import * as app from "../app.js";
+import * as helper from "../helper.js";
+import * as renderer from "../renderer.js";
+
+const router = express.Router();
+router.use(cors());
+
+router.all("/:itemId?", cors(), async (req, res, next) => {
+  try {
+    const itemId = req.params.itemId?.toUpperCase() || null;
+    if (!req.query.pack && req.cookies.pack) {
+      req.query.pack = req.cookies.pack;
+    }
+
+    const item = await renderer.renderItem(itemId, req.query, db);
+
+    if (item.error) {
+      throw new Error(item.error);
+    }
+
+    if (item.path) {
+      res.set("X-Texture-Path", `${item.path}`);
+    }
+
+    res.set("X-Cluster-ID", `${helper.getClusterId()}`);
+    res.setHeader("Cache-Control", `public, max-age=${app.CACHE_MAX_AGE}`);
+    res.contentType(item.mime);
+    res.send(item.image);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.use((err, req, res, next) => {
+  handleError(res, err);
+});
+
+export function handleError(res, err, status = 500, logged = true) {
+  if (logged) {
+    console.error(err);
+  }
+
+  res.status(status).send(err.message);
+}
+
+export { router };
