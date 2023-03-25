ea6d4b1ec41070abbbd67f369c144155c2d844c3
DuckySoLucky <robertkovac160@gmail.com>
2023-03-25 16:45:25 +0100

    fix: admin rarity missing background color

--- common/constants/items.js
+++ common/constants/items.js
@@ -12,6 +12,7 @@ export const RARITIES = [
   "supreme",
   "special",
   "very_special",
+  "admin",
 ];
 
 /** @typedef {"0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"|"a"|"b"|"c"|"d"|"e"|"f"} ColorCode */
