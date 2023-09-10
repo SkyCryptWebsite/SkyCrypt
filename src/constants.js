01ac7ce1be9c5b6aa57b13420027119d107c79ff
DuckySoLucky <robertkovac160@gmail.com>
2023-09-10 11:40:12 +0200

    fix: oops

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,6 @@ export * from "../common/constants.js";
 export * from "./constants/armor.js";
 export * from "./constants/bestiary.js";
 export * from "./constants/bingo.js";
-export * from "./constants/collections.js";
 export * from "./constants/dungeons.js";
 export * from "./constants/forge.js";
 export * from "./constants/gemstones.js";
