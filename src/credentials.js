9b2a9af8f40e9adad1eab367ca9907dfb6a3dbb5
Shiiyu <40838999+Shiiyu@users.noreply.github.com>
2024-02-02 18:18:03 -0500

    Fix issue with redis credentials

--- src/credentials.js
+++ src/credentials.js
@@ -15,12 +15,7 @@ const defaultCredentials = {
   hypixel_api_key: "",
   dbUrl: "mongodb://localhost:27017",
   dbName: "sbstats",
-  redisUrl: {
-    port: 6379,
-    host: "redis://localhost:6379",
-    username: "default",
-    password: "password",
-  },
+  redisUrl: "redis://localhost:6379",
   get session_secret() {
     return randomBytes(32).toString("hex");
   },
