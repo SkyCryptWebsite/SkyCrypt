bda9c5c4e064d322ce9471bcd038ec236dc1ad15
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-05-09 21:53:43 +0200

    feat: update tailwind.config.ts with new textShadow and animation styles

--- svelte/tailwind.config.ts
+++ svelte/tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 export default {
   darkMode: ["class"],
   content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -18,32 +19,57 @@ export default {
         background: {
           DEFAULT: "hsl(var(--background) / <alpha-value>)",
           grey: "hsl(var(--grey_background) / <alpha-value>)",
-          lore: "var(--lore_background)",
+          lore: "var(--lore_background)"
         },
         header: "hsl(var(--header) / <alpha-value>)",
         minecraft: {
-          0: "var(--$0)",
-          1: "var(--$1)",
-          2: "var(--$2)",
-          3: "var(--$3)",
-          4: "var(--$4)",
-          5: "var(--$5)",
-          6: "var(--$6)",
-          7: "var(--$7)",
-          8: "var(--$8)",
-          9: "var(--$9)",
-          a: "var(--$a)",
-          b: "var(--$b)",
-          c: "var(--$c)",
-          d: "var(--$d)",
-          e: "var(--$e)",
-          f: "var(--$f)"
+          0: "var(--§0)",
+          1: "var(--§1)",
+          2: "var(--§2)",
+          3: "var(--§3)",
+          4: "var(--§4)",
+          5: "var(--§5)",
+          6: "var(--§6)",
+          7: "var(--§7)",
+          8: "var(--§8)",
+          9: "var(--§9)",
+          a: "var(--§a)",
+          b: "var(--§b)",
+          c: "var(--§c)",
+          d: "var(--§d)",
+          e: "var(--§e)",
+          f: "var(--§f)"
         }
       },
       bg: "var(--bg)",
-      logo: "var(--logo)"
+      logo: "var(--logo)",
+      textShadow: {
+        sm: "0 0px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 0px 4px var(--tw-shadow-color)",
+        lg: "0 0px 8px var(--tw-shadow-color)"
+      },
+      animation: {
+        shine: "shine 4s infinite"
+      },
+      keyframes: {
+        shine: {
+          "0%": { transform: "translateY(120%)" },
+          "33%, 100%": { transform: "translateY(-120%)" }
+        }
+      }
     }
   },
 
-  plugins: []
-};
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value
+          })
+        },
+        { values: theme("textShadow") }
+      );
+    })
+  ]
+} satisfies Config;
