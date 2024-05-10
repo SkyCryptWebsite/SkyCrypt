16f987f9587fde484cd98662aad740e640336dcf
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-05-10 14:16:36 +0200

    feat: add iconmoon font

--- svelte/tailwind.config.ts
+++ svelte/tailwind.config.ts
@@ -56,6 +56,9 @@ export default {
           "0%": { transform: "translateY(120%)" },
           "33%, 100%": { transform: "translateY(-120%)" }
         }
+      },
+      fontFamily: {
+        icomoon: ["icomoon"]
       }
     }
   },
